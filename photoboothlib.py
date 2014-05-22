694fff06c73f79ec978de6a7b45d019d4870022f
r14793 <r14793@r14793ubuntu.(none)>
2014-05-22 15:17:25 -0600

    command line arg processing in photobooth.py

--- photoboothlib.py
+++ photoboothlib.py
@@ -8,13 +8,15 @@ import pygame
 from string import split,join
 from pygame.locals import *
 
+# list with raw image suffixes, used for appending to files as they are created
 suffix = [ 'a', 'b', 'c', 'd' ]
 
+# execute a shell command, printing it to the console for debugging purposes...
 def shellcmd(command):
 	print ' =>', command
 	os.system(command)
 
-# filename function: returns next sequential filename
+# new filename generation function: returns next sequential filename
 def new_filename(storename='lastphoto', increment=True):
 	last = eval(open(storename, 'r').read())
 	filename = 'DSC' + (4-len(str(last)))*'0' + str(last)
@@ -22,6 +24,8 @@ def new_filename(storename='lastphoto', increment=True):
 	open(storename, 'w').write(str(last))
 	return filename
 
+
+# dictionary to store templates for composite images, basically sizes, locations, and temp file names
 templates = {'display':[ ['504x336', '+124+12', 'images/backscreen-hd.jpg', 'boutput.jpg'],
 			['504x336', '+124+373', 'boutput.jpg', 'boutput.jpg'],
 			['504x336', '+652+12', 'boutput.jpg', 'boutput.jpg'],
@@ -36,7 +40,7 @@ templates = {'display':[ ['504x336', '+124+12', 'images/backscreen-hd.jpg', 'bou
 		}	
 
 # generate a composite image from a template, horizontal or vertical, abstracted 
-# and genericized so that various combinations of templates can be created...
+#    and genericized so that various combinations of templates can be created...
 def generate_composite(template, filename, blocking=False, generateprint=False):
 	# base graphicsmagick commands to generate composites...
 	interrim = 'gm composite -resize &ARG1 -geometry &ARG2 &FILENAME_&I.jpg &ARG3 -quality 98 &ARG4 '
@@ -60,18 +64,31 @@ def generate_composite(template, filename, blocking=False, generateprint=False):
 		command = join(split(command, '&ARG'+str(j+1)), templates[template][4][j])
 	shellcmd(command)
 
-	if generateprint: # do the extra step to make a double print strip...
+	# do this extra step to make a double print strip, 
+	#    assumes use of 'phone' template/ending file...
+	#    (requires a vertical 2000x6000 final composite image to start)
+	if generateprint: 
 		shellcmd('gm composite -geometry +0+0 ' + filename + '_phone.jpg images/background-big.jpg -quality 100 done.jpg')
 		shellcmd('gm composite -geometry +2001+0 ' + filename + '_phone.jpg done.jpg -quality 100 done.jpg')
 		shellcmd('gm convert -stroke gray -draw "line 2000,0 2000,6000" done.jpg -quality 95 ' + filename + '_print.jpg')
 
 
-
+# delete the temporary files created during creation of composites...
 def cleanup_temp_files(filename):
 	# remove temp files...
 	shellcmd('rm *output.jpg done.jpg '+filename+'*done')
 
+# filename function: returns next sequential filename (as a string)
+def new_filename(storename='lastphoto', increment=True):
+	last = eval(open(storename, 'r').read())
+	filename = 'DSC' + (4-len(str(last)))*'0' + str(last)
+	if increment: last=last+1
+	open(storename, 'w').write(str(last))
+	return filename
 
+# function to grab the sequence of raw images. 
+#    copied dummy images to a new 'filename' for testing 
+#    so that camera does not need to be used...
 def grab_image(filename, i, usecamera=True):
 	# Only capture image if it's one of the four... 
 	if i in range(4): 
@@ -79,4 +96,8 @@ def grab_image(filename, i, usecamera=True):
 		if usecamera: C.capture_image(filename+'_'+suffix[i] + '.jpg')
 		else: shellcmd('cp images/DSCdummy'+str(i+1)+'.jpg '+filename+'_'+suffix[i] + '.jpg')
 	open(filename+'_'+suffix[i]+'_done', 'w').write('done') # flag that file is complete...
+
+
+
+
 
